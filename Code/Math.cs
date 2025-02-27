d03cb26a4b1102cca98cf8ffcd5ad6e5aef66ae7
Angelo Belchior <angelo.luis@picpay.com>
2025-02-27 17:56:38 -0300

    General Adjustments

--- Code/Math.cs
+++ Code/Math.cs
@@ -1,36 +1,38 @@
+namespace _4DVectors.Code;
+
 public class Math
 {
-    public const float pi = 3.14159274;
     public static float SquareRoot(float number)
     {
         // Newton's Method
-        float accuracy = 1e-6f; // 10^-6, or the function is accurate to 6 decimal places
+        var accuracy = 1e-6f; // 10^-6, or the function is accurate to 6 decimal places
         float prevApprox; // Used to calculate if it is close to the tolerance
         float approx;
-     
-        if(number == 0)
-           return 0;
-        else if(number < 1)
-          approx = 1;
-        else
-           approx = number / 2;
 
-         // This function will return a square root with an accuracy to 6 decimal places, however if that isn't possible or we don't reach that after 100 iterations, it'll just return what it has. This ensures that we don't get stuck in an infinite loop
-        for(int i = 0; i < 100; i++)
+        if (number == 0)
+            return 0;
+        if (number < 1)
+            approx = 1;
+        else
+            approx = number / 2;
+
+        // This function will return a square root with an accuracy to 6 decimal places, however if that isn't possible or we don't reach that after 100 iterations, it'll just return what it has. This ensures that we don't get stuck in an infinite loop
+        for (var i = 0; i < 100; i++)
         {
             prevApprox = approx;
-            approx = 0.5f * (approx + (number/approx));
-            if(AbsoluteValue(approx - prevApprox) < accuracy)
-              return approx;
+            approx = 0.5f * (approx + number / approx);
+            if (AbsoluteValue(approx - prevApprox) < accuracy)
+                return approx;
         }
+
         return approx;
     }
 
     public static float AbsoluteValue(float number)
     {
         // If the number is negative, return the number multiplied by -1 (which makes it positive)
-        if(number < 0)
-         return -number;
+        if (number < 0)
+            return -number;
         // Else just return the number
         return number;
     }
@@ -39,43 +41,47 @@ public class Math
     public static float CosineRad(float value)
     {
         // Using taylor's series, will be accurate to decimal places
-        return 1 - ((value * value) / 2) + ((value * value * value * value) / 24) - ((value * value * value * value * value * value) / 720);
+        return 1 - value * value / 2 + value * value * value * value / 24 -
+               value * value * value * value * value * value / 720;
     }
-    
+
     // Returns cosine in Degrees
     public static float CosineDeg(float value)
     {
-        value = value * (pi / 180);
+        value *= (float.Pi / 180);
         // Using taylor's series, will be accurate to decimal places
-        return (180 / pi) * (1 - ((value * value) / 2) + ((value * value * value * value) / 24) - ((value * value * value * value * value * value) / 720));
+        return 180 / float.Pi * (1 - value * value / 2 + value * value * value * value / 24 -
+                                 value * value * value * value * value * value / 720);
     }
 
     // Returns sine in Radians
     public static float SineRad(float value)
     {
         // Using taylor's series, will be accurate to decimal places
-        return value - ((value * value * value)/6) + ((value * value * value * value * value) / 120) - ((value * value * value * value * value * value * value) / 5040);
+        return value - value * value * value / 6 + value * value * value * value * value / 120 -
+               value * value * value * value * value * value * value / 5040;
     }
-    
-        // Returns sine in Degrees
+
+    // Returns sine in Degrees
     public static float SineDeg(float value)
     {
-        value = value * (pi / 180);
+        value *= (float.Pi / 180);
         // Using taylor's series, will be accurate to decimal places
-        return (180 / pi) * (value - ((value * value * value)/6) + ((value * value * value * value * value) / 120) - ((value * value * value * value * value * value * value) / 5040));
+        return 180 / float.Pi * (value - value * value * value / 6 + value * value * value * value * value / 120 -
+                                 value * value * value * value * value * value * value / 5040);
     }
 
     public static float ArccosineDeg(float value)
     {
         // Newton's Method
-        float accuracy = 1e-6f; // 10^-6, or the function is accurate to 6 decimal places
+        var accuracy = 1e-6f; // 10^-6, or the function is accurate to 6 decimal places
         float prevApprox; // Used to calculate if it is close to the tolerance
         float approx = 1;
 
-        for(int i = 0; i < 100; i++) 
+        for (var i = 0; i < 100; i++)
         {
             prevApprox = approx;
-            approx = approx + CosineDeg(approx) / -SineDeg(approx);
+            approx += CosineDeg(approx) / -SineDeg(approx);
             if (AbsoluteValue(approx - prevApprox) < accuracy)
                 return approx;
         }
